79542ccce3cef5c4c7461a0ffafcd9f040024728
Yoon Gwan <blackgar@naver.com>
2023-01-08 20:43:48 +0900

    4주차 문제 풀이

--- Ken/3주차/Lv2/[1차]캐시(객체, 반례존재).js
+++ Ken/3주차/Lv2/[1차]캐시(객체, 반례존재).js
@@ -11,17 +11,17 @@ function solution(cacheSize, cities) {
   // jeju : 1 => jeju : 2, pangyo : 1 => jeju : 3, pangyo : 2, seoul : 1 => jeju : 1, pangyo : 3, seoul : 2 이런식으로 진행
   // jeju pangyo seoul newyork la jeju pangyo seoul newyork input을 예시로 하면
   // jeju : 1 => jeju : 2, pangyo : 1 => jeju : 3, pangyo : 2, seoul : 1 => jeju : 4이므로 delete, pangyo : 3, seoul : 2, newyork : 1 => 다음은 pangyo : 4이므로 delete seoul : 3, newyork : 2, la : 1 이런식으로 진행
-  console.log(cacheSize, cities);
-  for (const city of cities) {
+  // console.log(cacheSize, cities);
+  for (let city of cities) {
     city = city.toLowerCase();
-    console.log(cacheObj[city]);
+    // console.log(cacheObj[city]);
     // if (cacheObj[city] > cacheSize) delete cacheObj[city];
     cacheObj[city] ? (answer += 1) : (answer += 5);
     cacheObj[city] = 0;
     Object.keys(cacheObj).forEach((v) =>
       cacheObj[v] + 1 > cacheSize ? delete cacheObj[v] : cacheObj[v]++
     );
-    console.log(cacheObj);
+    // console.log(cacheObj);
   }
 
   return answer;
@@ -91,5 +91,5 @@ console.log(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]));
 console.log(solution(3, ["A", "B", "A"])); // 11
 console.log(solution(10, ["1", "2", "3", "2"])); // 16
 console.log(solution(0, ["A", "B", "A", "A", "A"])); // 25
-console.log(solution(1, ["A", "B", "A", "B", "A"])); // 17
+console.log(solution(1, ["A", "B", "A", "A", "A"])); // 17
 console.log(solution(2, ["A", "B", "A", "A", "A"])); // 13
