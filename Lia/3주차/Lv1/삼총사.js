e6d92a6662350431c2bb5d01f6c6846a694ba312
huisso97 <chosohi@naver.com>
2023-01-09 22:41:08 +0900

    삼총사, 올바른 괄호

--- Lia/3주차/Lv1/삼총사.js
+++ Lia/3주차/Lv1/삼총사.js
@@ -1,22 +1,38 @@
 function solution(number) {
   let answer = 0;
-  function summation(arr, start) {
-    combination = 0;
-    if (arr.length === 3) {
-      for (const num in arr) {
-        combination += num;
+  for (let i = 0; i < number.length - 2; i++) {
+    for (let j = i + 1; j < number.length - 1; j++) {
+      for (let k = j + 1; k < number.length; k++) {
+        const tmp = number[i] + number[j] + number[k];
+
+        if (tmp === 0) answer++;
       }
-      if (combination === 0) {
-        answer++;
-      }
-      return;
-    }
-    for (let i = start; i < number.length; i++) {
-      solution(number, i + 1);
     }
   }
-  summation(number, 0);
   return answer;
 }
 
-console.log(solution([-2, 3, 0, 2, -5]));
+console.log(solution([-3, -2, -1, 0, 1, 2, 3]));
+
+// 다른 사람 풀이
+
+function solution(number) {
+  let result = 0;
+
+  const combination = (current, start) => {
+    if (current.length === 3) {
+      result += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+      return;
+    }
+
+    for (let i = start; i < number.length; i++) {
+      combination([...current, number[i]], i + 1);
+    }
+  };
+  combination([], 0);
+  return result;
+}
+
+// 서로 다른 사람 3명을 필요로하므로
+// 조합 활용 => 3명이 존재 => 그 합을 구하여 0이면 count++;
+// combination 함수 사용
--- Lia/4주차/Lv2/JadenCase 문자열 만들기.js
+++ Lia/4주차/Lv2/JadenCase 문자열 만들기.js
@@ -1,15 +1,6 @@
 function solution(s) {
   var answer = "";
-  let string = s;
-  let index = 0;
-  let arr = string.toLowerCase();
-  for (let i = 0; i < arr.length; i++) {
-    if (index === 0) {
-      arr[i].toUpperCase();
-    }
-    index++;
-  }
-  console.log(arr);
+
   return answer;
 }
-console.log(solution("3people Unfollowed Me"));
+console.log(solution("3people unFollowed me"));
