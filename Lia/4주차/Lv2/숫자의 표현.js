b20214e7b2e7efc2ec8c3f621da045152a30a6c5
huisso97 <chosohi@naver.com>
2023-01-11 21:24:16 +0900

    짝지어 제거하기

--- Lia/4주차/Lv2/숫자의 표현.js
+++ Lia/4주차/Lv2/숫자의 표현.js
@@ -1,23 +1,7 @@
 function solution(s) {
   var answer = 0;
 
-  for (let n = 0; n < parseInt(s / 2) + 1; n++) {
-    let res = recurSum(n, n, s);
-    console.log(res);
-    if (res) {
-      answer++;
-    }
-  }
   return answer;
 }
-function recurSum(cur, sum, s) {
-  if (sum === s) {
-    // console.log(sum);
-    return true;
-  }
-  if (sum > s) return false;
-  if (sum < s) {
-    return recurSum(cur + 1, cur + 1 + sum, s);
-  }
-}
+
 console.log(solution(15));
