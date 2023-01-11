3d2c3287b60e8a72946d4c14284a925b743e0a9a
huisso97 <chosohi@naver.com>
2023-01-11 22:44:16 +0900

    연속 부분 수열 합의 개수

--- Lia/2주차/Lv2/연속 부분 수열 합의 개수.js
+++ Lia/2주차/Lv2/연속 부분 수열 합의 개수.js
@@ -1,14 +1,24 @@
-// sliding window
 function solution(elements) {
-  let answer = 0;
   // 세는 단위 : 1 ~ elements.length
   let sum = new Set();
-  for (let i = 1; i < elements.length + 1; i++) {
-    // 최초 합 구하기
-    let firstSum = elements.slice(0, i);
-    console.log(firstSum);
+  for (let cnt = 1; cnt <= elements.length; cnt++) {
+    let tmp = [];
+    for (let i = 0; i < elements.length; i++) {
+      if (i + cnt > elements.length) {
+        tmp = [
+          ...elements.slice(i),
+          ...elements.slice(0, i + cnt - elements.length),
+        ];
+      } else {
+        tmp = elements.slice(i, i + cnt);
+      }
+
+      let res = tmp.reduce((acc, val) => acc + val, 0);
+      sum.add(res);
+    }
   }
-  return answer;
+
+  return sum.size;
 }
 
 solution([7, 9, 1, 1, 4]);
