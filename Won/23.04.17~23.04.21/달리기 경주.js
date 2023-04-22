dbeaf4e354a559783a0c911c0626a03679760d27
wonjongjang <bjlaancgk@gmail.com>
2023-04-22 10:08:34 +0900

    [PGS] 달리기 경주 23.04.22

--- Won/23.04.17~23.04.21/달리기 경주.js
+++ Won/23.04.17~23.04.21/달리기 경주.js
@@ -1,3 +1,25 @@
+function solution(players, callings) {
+  const dict = {}; // name: rank
+
+  // 플레이어들의 이름 별 현재 등수 저장
+  players.forEach((v, i) => {
+    dict[v] = i;
+  });
+
+  // 선수 이름 부를때마다 dict과 players 순서 변경
+  callings.forEach((v) => {
+    const idx = dict[v]; // 부른 선수의 현재 등수
+    const temp = players[idx - 1]; // 부른 선수의 바로 앞 선수 이름
+
+    dict[v]--; // 부른 선수 등수 -1
+    dict[temp]++; // 부른 선수의 바로 앞 선수 등수 +1
+
+    players[idx - 1] = v; // 부른 선수 등수 -1
+    players[idx] = temp; // 부른 선수의 바로 앞 선수 등수 +1
+  });
+  return players;
+}
+
 // 시간 초과 (10 ~ 13)
 function solution(players, callings) {
   callings.forEach((v) => {
