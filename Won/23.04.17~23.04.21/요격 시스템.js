1c1a71c871600a4752bb0a2f8e9a32d43a687fae
wonjongjang <bjlaancgk@gmail.com>
2023-04-24 20:51:38 +0900

    [PGS] 요격 시스템 23.04.24

--- Won/23.04.17~23.04.21/요격 시스템.js
+++ Won/23.04.17~23.04.21/요격 시스템.js
@@ -1,1 +1,17 @@
 //
+function solution(targets) {
+  // s 기준 내림차순 정렬
+  targets.sort((a, b) => {
+    return b[0] - a[0];
+  });
+  let result = 1,
+    check = targets.shift()[0]; // result: 요격 미사일 수, check: 첫번째 s
+  targets.forEach((v, i) => {
+    // 다음 타켓의 e <= check(이전 타겟의 s) -> 범위가 겹치지 않으면
+    if (v[1] <= check) {
+      check = v[0];
+      result++; // 미사일 개수 추가
+    }
+  });
+  return result;
+}
