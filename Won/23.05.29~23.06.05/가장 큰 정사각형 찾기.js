e2a3e2763ec84ea35cb3e05bd9586c9602c5e4e9
wonjongjang <bjlaancgk@gmail.com>
2023-06-14 22:49:28 +0900

    [PGS] 가장 큰 정사각형 찾기 23.06.14

--- Won/23.05.29~23.06.05/가장 큰 정사각형 찾기.js
+++ Won/23.05.29~23.06.05/가장 큰 정사각형 찾기.js
@@ -1,14 +1,18 @@
 function solution(board) {
+  const row = board.length; // 행의 크기
+  const col = board[0].length; // 열의 크기
+
+  if (row < 2 || col < 2) return 1; // 행과 열이 1 이하인 경우
+
   let maxLength = 0;
-  for (let i = 1; i < board.length; i++) {
-    for (let j = 1; j < board[i].length; j++) {
+  for (let i = 1; i < row; i++) {
+    for (let j = 1; j < col; j++) {
       if (board[i][j] === 1) {
         const temp =
           Math.min(board[i - 1][j], board[i][j - 1], board[i - 1][j - 1]) + 1;
         board[i][j] = temp;
         maxLength = Math.max(maxLength, temp);
       }
-      // console.log(board[i][j], board[i-1][j])
     }
   }
   return maxLength ** 2;
